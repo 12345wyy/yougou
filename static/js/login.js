c15ccc0d4f5d9e5b83d83f98d75544f457a6975b
12345wyy <1669058635@qq.com>
2018-11-29 19:30:49 +0800

    xxx

---
+++ static/js/login.js
@@ -0,0 +1,19 @@
+var checkInput = function () {
+
+},
+var login = (function () {
+    return {
+        init: function () {
+            this.$form = document.querySelector("form");
+            this.$username = this.$form.querySelector(".username");
+            console.log(this.$username);
+            this.event();
+        },
+        event: function () {
+            var _this = this;
+        },
+        checkFrom: function () {
+
+        }
+    }
+}())
